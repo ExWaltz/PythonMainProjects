7f4e624a0884b84e461b5f482e622627fde82d63
ExWaltz <minecraftexwaltz@gmail.com>
2020-09-11 22:57:47 +0800

    Add new Dict to QuizBook Function. Add: Add A quizlet to QuizBook, Remove: Remove Quizlet from QuizBook, GetQuizInfo: Get question and answer from QuizBook, GetAllQuizInfo: Get all questions and answer from QuizBook, GetQuestions: Get all questions in QuizBook

--- Quizlet/Quiz.py
+++ Quizlet/Quiz.py
@@ -9,7 +9,7 @@ import random
 
 
 class PythonQuiz:
-    def QuizBook(self, bookname, question=None, choices=[], answer=0, mode=1, qrandom=False, recordTime=True):
+    def QuizBook(self, bookname, question=None, choices=[], answer=0, mode=0, qrandom=False, recordTime=True):
         ''' bookname:   # Required
                 * Enter existing or new Quiz Book name
             question:   # Optional
@@ -30,33 +30,63 @@ class PythonQuiz:
         '''
         try:
             qbStaus = self._checkQuizBook(bookname)
-            f_bookname = self._checkBookName(bookname)
-            f_answer = json.dumps(answer)
+            fixed_bookname = self._checkBookName(bookname)
+            fixed_answer = json.dumps(answer)
+            fixed_random = json.dumps(qrandom)
+            fixed_recordTime = json.dumps(recordTime)
+            fixed_choices = json.dumps(choices)
 
             if mode == 0:
                 choices = [True, False]
             elif mode == 1:
-                f_answer = json.dumps(choices[int(answer)])
+                fixed_answer = json.dumps(choices[int(answer)])
 
             if qbStaus:
-                jsnFile = open(str(f_bookname), 'w', encoding="utf-8")
+                jsnFile = open(str(fixed_bookname), 'w', encoding="utf-8")
                 self.AddQuizToList(os.path.realpath(jsnFile.name))
                 if question is None:
-                    data = f'{{"{bookname}":[{{"random": {random}, "recordTime": {recordTime}}}]}}'
+                    data = f'{{"{bookname}":[{{"random": {fixed_random}, "recordTime": {fixed_recordTime}}}]}}'
                 else:
-                    data = f'{{"{bookname}": [{{"{question}": [{{"choices": {json.dumps(choices)}, "answer": {f_answer}, "mode": {mode}, "time": null}}], "random": {json.dumps(qrandom)}, "recordTime": {json.dumps(recordTime)}}}]}}'
+                    data = f'{{"{bookname}": [{{"{question}": [{{"choices": {fixed_choices}, "answer": {fixed_answer}, "mode": {mode}, "time": null}}], "random": {fixed_random}, "recordTime": {fixed_recordTime}}}]}}'
                 jsnLoad = json.loads(data)
                 json.dump(jsnLoad, jsnFile, indent=2)
                 jsnFile.close()
             elif question is not None:
                 self.UpdateQuizBook(bookname, question,
                                     choices, answer, mode)
+
+            # Quizlet options
+            def AddQuizlet(qquestion, qchoice=[], qanswer=0, qmode=0):
+                if qmode == 0:
+                    qchoice = [True, False]
+                self.UpdateQuizBook(bookname, qquestion,
+                                    qchoice, qanswer, qmode)
+
+            def RemoveQuizlet(qquestion):
+                self.DeleteQuizlet(bookname, qquestion)
+
+            def GetQuizInfo(qquestion):
+                return self.getQuizletInfo(bookname, qquestion)
+
+            def GetAllQuizInfo():
+                return self.getAllQuizletInfo(bookname)
+
+            def GetQuestions():
+                return self.getQuestions(bookname)
+
+            self.QuizBook.__dict__["Add"] = AddQuizlet
+            self.QuizBook.__dict__["Remove"] = RemoveQuizlet
+            self.QuizBook.__dict__["GetQuizInfo"] = GetQuizInfo
+            self.QuizBook.__dict__["GetAllQuizInfo"] = GetAllQuizInfo
+            self.QuizBook.__dict__["GetQuestions"] = GetQuestions
+
+            return self.QuizBook
         except Exception as e:
             raise e("TypeError: Invalid Type")
 
     def QuizBookOptions(self, bookname, qrandom=False, recordTime=True):
-        f_bookname = self._checkBookName(bookname)
-        jsnFile = open(str(f_bookname), 'r+', encoding="utf-8")
+        fixed_bookname = self._checkBookName(bookname)
+        jsnFile = open(str(fixed_bookname), 'r+', encoding="utf-8")
         data = json.loads(jsnFile.read())
         data[bookname][0]["random"] = qrandom
         data[bookname][0]["recordTime"] = recordTime
@@ -66,16 +96,15 @@ class PythonQuiz:
         jsnFile.close()
 
     def UpdateQuizBook(self, ubookname, uquestion, uchoices=[], uanswer=0, umode=1):
-        f_ubookname = self._checkBookName(ubookname)
-        f_answer = uanswer
+        fixed_ubookname = self._checkBookName(ubookname)
+        fixed_answer = uanswer
         if umode == 1:
-            f_answer = uchoices[int(uanswer)]
-        print(f_answer)
-        jsnFile = open(str(f_ubookname), 'r+', encoding="utf-8")
+            fixed_answer = uchoices[int(uanswer)]
+        jsnFile = open(str(fixed_ubookname), 'r+', encoding="utf-8")
         self.AddQuizToList(os.path.realpath(jsnFile.name))
         data = json.loads(jsnFile.read())
         uquestionData = {"choices": uchoices,
-                         "answer": f_answer, "mode": umode, "time": None}
+                         "answer": fixed_answer, "mode": umode, "time": None}
         if data[str(ubookname)][0].get(str(uquestion)) is None:
             data[str(ubookname)][0][str(uquestion)] = [uquestionData]
         data[str(ubookname)][0][str(uquestion)][0] = uquestionData
@@ -84,11 +113,24 @@ class PythonQuiz:
         jsnFile.truncate()
         jsnFile.close()
 
+    def DeleteQuizlet(self, bookname, qquestion):
+        if self._verifyQuizlet(bookname, qquestion):
+            fixed_bookname = self._checkBookName(bookname)
+            jsnFile = open(str(fixed_bookname), 'r+', encoding="utf-8")
+            data = json.loads(jsnFile.read())
+            del data[bookname][0][qquestion]
+            jsnFile.seek(0)
+            json.dump(data, jsnFile, indent=2)
+            jsnFile.truncate()
+            jsnFile.close()
+        else:
+            raise Exception("Question does not exist")
+
     def AddQuizToList(self, bookname):
-        fixbookname = str(bookname).replace("\\", "/")
+        bookpath = str(bookname).replace("\\", "/")
         if not os.path.exists("quizList.json"):
             savequizList = open("quizList.json", "w", encoding="utf-8")
-            data = f'{{"QuizList": ["{str(fixbookname)}"]}}'
+            data = f'{{"QuizList": ["{str(bookpath)}"]}}'
             jsnLoad = json.loads(data)
             jsnData = json.dumps(jsnLoad, indent=2)
             savequizList.write(jsnData)
@@ -97,7 +139,7 @@ class PythonQuiz:
             savequizList = open("quizList.json", "r+", encoding="utf-8")
             data = json.loads(savequizList.read())
             quizList = data["QuizList"]
-            quizList.append(fixbookname)
+            quizList.append(bookpath)
             cleanQuizList = list(dict.fromkeys(quizList))
             data["QuizList"] = cleanQuizList
             savequizList.seek(0)
@@ -114,15 +156,14 @@ class PythonQuiz:
                 * Answers to question
             mode:       # Optional
                 * 0: True or False quizlet
-                * 1: Multiple Choice quizlet # Default
+                * 1: Multiple Choice quizlet
                 * 2: Input quizlet
-                * mode > 2: Multiple Choice quizlet
         '''
         self.QuizBook(quizname, question, choices, answer, mode)
 
     def ResetQuiz(self, qbookname, qquestion):
-        f_qbookname = self._checkBookName(qbookname)
-        jsnFile = open(str(f_qbookname), 'r+', encoding="utf-8")
+        fixed_qbookname = self._checkBookName(qbookname)
+        jsnFile = open(str(fixed_qbookname), 'r+', encoding="utf-8")
         data = json.loads(jsnFile.read())
         quizBookData = data.get(str(qbookname))[0]
         isRand = quizBookData.get("random")
@@ -139,13 +180,15 @@ class PythonQuiz:
         jsnFile.close()
 
     def getAllQuizletInfo(self, bookname):
-        f_bookname = self._checkBookName(bookname)
-        jsnFile = open(str(f_bookname), 'r+', encoding="utf-8")
+        fixed_bookname = self._checkBookName(bookname)
+        jsnFile = open(str(fixed_bookname), 'r+', encoding="utf-8")
         data = json.loads(jsnFile.read())
         quizBookData = data.get(bookname)[0]
         quizQuestions = list(dict.fromkeys(quizBookData))
         quizQuestions.remove("random")
         quizQuestions.remove("recordTime")
+        if len(quizQuestions) == 0:
+            return []
         isRand = quizBookData.get("random")
         if isRand:
             random.shuffle(quizQuestions)
@@ -159,8 +202,8 @@ class PythonQuiz:
         return quizInfo
 
     def getQuizletInfo(self, bookname, quizletname):
-        f_bookname = self._checkBookName(bookname)
-        jsnFile = open(str(f_bookname), 'r+', encoding="utf-8")
+        fixed_bookname = self._checkBookName(bookname)
+        jsnFile = open(str(fixed_bookname), 'r+', encoding="utf-8")
         data = json.loads(jsnFile.read())
         if data.get(bookname)[0].get(quizletname):
             quizQuestion = quizletname
@@ -171,8 +214,8 @@ class PythonQuiz:
             return compileInfo
 
     def getQuestions(self, bookname):
-        f_bookname = self._checkBookName(bookname)
-        jsnFile = open(str(f_bookname), 'r+', encoding="utf-8")
+        fixed_bookname = self._checkBookName(bookname)
+        jsnFile = open(str(fixed_bookname), 'r+', encoding="utf-8")
         data = json.loads(jsnFile.read())
         quizBookData = data.get(bookname)[0]
         quizQuestions = list(dict.fromkeys(quizBookData))
@@ -188,17 +231,6 @@ class PythonQuiz:
             return str(bookname) + ".quiz"
         return str(bookname)
 
-    def _verifyQuizBook(self, bookname):
-        try:
-            f_bookname = self._checkBookName(bookname)
-            jsnFile = open(str(f_bookname), 'r+', encoding="utf-8")
-            data = json.loads(jsnFile.read())
-            data[bookname][0]["random"]
-            data[bookname][0]["recordTime"]
-            return True
-        except Exception:
-            return False
-
     def _checkQuizBook(self, quizbookname):
         f_quizbookname = self._checkBookName(quizbookname)
         if os.path.exists(str(f_quizbookname)):
@@ -206,8 +238,33 @@ class PythonQuiz:
                 return False
         return True
 
+    def _verifyQuizBook(self, bookname):
+        try:
+            fixed_bookname = self._checkBookName(bookname)
+            jsnFile = open(str(fixed_bookname), 'r+', encoding="utf-8")
+            data = json.loads(jsnFile.read())
+            data[bookname][0]["random"]
+            data[bookname][0]["recordTime"]
+            return True
+        except Exception:
+            return False
+
+    def _verifyQuizlet(self, bookname, quizletname):
+        try:
+            fixed_bookname = self._checkBookName(bookname)
+            jsnFile = open(str(fixed_bookname), 'r+', encoding="utf-8")
+            data = json.loads(jsnFile.read())
+            data[bookname][0][quizletname][0]["choices"][0]
+            data[bookname][0][quizletname][0]["answer"]
+            data[bookname][0][quizletname][0]["mode"]
+            data[bookname][0][quizletname][0]["time"]
+            return True
+        except Exception:
+            return False
+
 
 quiz = PythonQuiz()
 # quiz.QuizBookOptions(bookname="Test", qrandom=True)
-quiz.CreateQuizlet("Test", "Watame ch", answer=False, mode=0)
+test = quiz.QuizBook("Cool")
+test.Add("Wata")
 print(quiz.getAllQuizletInfo("Test"))
