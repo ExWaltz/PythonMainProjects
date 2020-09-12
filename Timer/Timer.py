2f64da46d0ed4ee16c6a83edca6edcfc50086cc6
ExWaltz <minecraftexwaltz@gmail.com>
2020-09-12 23:32:54 +0800

    Started on Timer Project

---
+++ Timer/Timer.py
@@ -0,0 +1,24 @@
+import time
+
+
+def Timer(hours, minutes, seconds, prnt=False):
+    currentTime = time.strftime("%H:%M:%S", time.localtime())
+    intTime = [int(e) for e in currentTime.split(':')]
+    timeOffset = [int(hours), int(minutes), int(seconds)]
+    timelimit = [curTime + offTime for curTime, offTime in zip(intTime, timeOffset)]
+    if timelimit[0] >= 24:
+        timelimit[0] = timelimit[0] - 24
+    for num, limit in enumerate(timelimit[1:]):
+        while limit >= 60:
+            timelimit[num + 1] = limit - 60
+            timelimit[num] += 1
+            limit -= 60
+    while timelimit > intTime:
+        currentTime = time.strftime("%H:%M:%S", time.localtime())
+        intTime = [int(e) for e in currentTime.split(':')]
+        timeLeft = [tLimit - curTime for curTime, tLimit in zip(intTime, timelimit)]
+        if prnt:
+            h, m, s = timeLeft
+            sTime = "{}:{}:{}".format(h, m, s)
+            print(sTime)
+    return True
