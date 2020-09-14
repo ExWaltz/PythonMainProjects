a1473d29abacd8a60c8aafd953564f25fa5d9c74
ExWaltz <minecraftexwaltz@gmail.com>
2020-09-14 22:17:05 +0800

    Half way there

--- Quizlet/QuizGui.py
+++ Quizlet/QuizGui.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+import random
 from Quiz import PythonQuiz as qb
 
 
@@ -40,6 +41,35 @@ class App():
         self._main_Content.__dict__["frame"] = frameContent
         return self._main_Content
 
+    def _startQuiz(self, quizInfo):
+        self.questionFrame.forget()
+        self.holdquiz = tk.Frame(self.mainFrame)
+        quizFrame = tk.Frame(self.holdquiz)
+        choiceFrame = tk.Frame(self.holdquiz)
+        self.holdquiz.pack(expand=1)
+        quizFrame.pack(side="top")
+        choiceFrame.pack(side="top")
+        self.root.update()
+        self.correct = False
+        for eq in quizInfo:
+            question = tk.Label(quizFrame, text=eq[0])
+            question.pack()
+            for choice in eq[1]:
+                print(choice)
+                # not properly adding choices
+                choices = tk.Button(choiceFrame, text=choice, command=lambda: self._answer(choice, eq[2]))
+                choices.pack()
+            while not self.correct:
+                self.root.update_idletasks()
+                self.root.update()
+
+    def _answer(self, choice, answer):
+        print([choice, answer])
+        if choice == answer:
+            self.correct = True
+            return
+        self.correct = False
+
     def _openQuiz(self, title, quizInfo):
         self.contentHolder.forget()
         self.mainText.config(text=title)
@@ -48,21 +78,22 @@ class App():
         quizBookFrame = tk.Frame(self.questionFrame, bg="blue", width=2000, bd=100)
         quizBookFrame.pack(side="top")
         self.root.update_idletasks()
-        if len(quizInfo) != 0:
+        if len(quizInfo[0]) != 0:
             nquizText = f"{len(quizInfo[0])} quiz"
             nquizInfo = f"Random: {quizInfo[1][1]}\nRecord Time: {quizInfo[1][2]}"
             numQuiz = tk.Label(quizBookFrame, text=nquizText, font=self.largeFont, wraplength=300)
             infoQuiz = tk.Label(quizBookFrame, text=nquizInfo, font=self.mainFont, wraplength=300, justify="left")
-            startButton = tk.Button(self.questionFrame, text="Start Quiz", height=3)
+            startButton = tk.Button(self.questionFrame, text="Start Quiz", height=3, command=lambda: self._startQuiz(quizInfo[0]))
             numQuiz.pack()
             infoQuiz.pack()
             startButton.pack(fill="x", pady=10)
             self.root.update()
         else:
             numQuiz = tk.Label(quizBookFrame, text="No Quizzes Found", font=self.titleFont, wraplength=300)
+            startButton = tk.Button(self.questionFrame, text="New Quizlet", height=3)
+            startButton.pack(fill="x", pady=10)
             numQuiz.pack(expand=1)
             self.root.update()
-        print(quizInfo)
 
     def _hLine(self, parent, bg_="#000000"):
         hrline = tk.Frame(parent, bg=bg_)
